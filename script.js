5a5b820f798f4a76880c0b31d0ae7e109e5f110d
Aaniket17 <aniketkumar.2000.17@gmail.com>
2024-08-28 22:59:45 +0530

    add css  javascript function and html

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.querySelectorAll('.service-box').forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        box.style.transform = 'translateY(-15px)';
+        box.style.transition = 'transform 0.4s ease';
+    });
+
+    box.addEventListener('mouseleave', () => {
+        box.style.transform = 'translateY(0)';
+    });
+});
